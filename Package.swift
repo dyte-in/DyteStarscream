26ea82c34d98b367468df3fa4f632caa86613c88
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-07-15 16:55:39 +0530

    fix: fixed checksum

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .binaryTarget(
             name: "Starscream",
             url: "https://github.com/dyte-in/DyteStarscream/archive/refs/tags/0.0.1.zip",
-            checksum: "98ae7326fb51c875c9671969967527f0574a031f4a7de39606c83e6f758ccfd9"
+            checksum: "8aa8a8078b74aa4dcb86bf27ce2b46ef51b5e5f731226cd30fbd83ac5d1e633f"
         )
     ]
 )
